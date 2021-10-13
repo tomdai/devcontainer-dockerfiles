172120b22c2f937623397a344194ca30086e989a
Tom Dai <5054148+tomdai@users.noreply.github.com>
2021-10-13 02:15:03 -0400

    Trigger rebuilding.

--- customize.sh
+++ customize.sh
@@ -259,5 +259,3 @@ zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 
 zstyle ':completion:*' keep-prefix true
 zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(\${=\${\${(f)"\$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
 END
-
-# trigger rebuilding
